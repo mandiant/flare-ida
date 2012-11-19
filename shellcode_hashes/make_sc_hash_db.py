1d0f7ffcdcd89d44f57eabffa711a23d151b0395
Jay Smith <jay.smith@mandiant.com>
2012-11-19 11:22:17 -0800

    Added missing license statement

--- shellcode_hashes/make_sc_hash_db.py
+++ shellcode_hashes/make_sc_hash_db.py
@@ -1,9 +1,23 @@
-#/usr/bin/env python
-#
-# Jerrold "Jay" Smith
+#!/usr/bin/env python
+# Jay Smith
 # jay.smith@mandiant.com
-# 5-28-2010
+# 
+########################################################################
+# Copyright 2012 Mandiant
 #
+# Mandiant licenses this file to you under the Apache License, Version
+# 2.0 (the "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at:
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+########################################################################
 # Traverse a directory, trying to find all exports for all valid PE
 # executable files. Computes common shellcode hashes and stores them
 # to a sqlite database file for later use, such as in IDA Pro.
