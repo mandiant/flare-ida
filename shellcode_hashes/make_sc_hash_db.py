fa7f6ae8f2e0085bc7fb42067c92060f7adcf193
Alex Berry <alex.berry@fireeye.com>
2016-02-25 10:59:53 -0500

    added dualaccmodfff1 hash

--- shellcode_hashes/make_sc_hash_db.py
+++ shellcode_hashes/make_sc_hash_db.py
@@ -557,6 +557,25 @@ for c in input_string {
 }
 '''
 
+def dualaccModFFF1Hash(inString,fName):
+    if inString is None:
+        return 0
+
+    v4, v8 = 0, 1
+    for ltr in inString:
+        v8 = (ord(ltr) + v8) % 0x0FFF1
+        v4 = (v4 + v8) % 0x0FFF1
+    return (v4 << 0x10)|v8
+
+pseudocode_dualaccModFFF1Hash = '''
+acc_1 := 0
+acc_2 := 0
+for c in input_string {
+    acc_2 = (acc_2 + c) % 0x0FFF1
+    acc_1 = (acc_1 + acc2) % 0x0FFF1
+}
+return (acc_1 << 0x10) | acc2
+'''
 
 
 
@@ -582,6 +601,7 @@ HASH_TYPES = [
     ('add1505Shl5Hash32',       32, pseudocode_add1505Shl5Hash32),
     ('rol7XorHash32',           32, pseudocode_rol7XorHash32),
     ('rol7AddXor2Hash32',       32, pseudocode_rol7AddXor2Hash32),
+    ('dualaccModFFF1Hash',       32, pseudocode_dualaccModFFF1Hash),
 ]
 
 
