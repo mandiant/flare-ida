c7db0348cb0f3913122c3f39d522022360288f81
Chuong Dong <chuong.dong@FireEye.com>
2021-08-06 06:37:37 -0700

    Add Conti API hashing

--- shellcode_hashes/make_sc_hash_db.py
+++ shellcode_hashes/make_sc_hash_db.py
@@ -45,7 +45,7 @@ INTERESTING_DLLS = [
     'ntoskrnl.exe', 'oleaut32.dll', 'psapi.dll',    'shell32.dll',
     'shlwapi.dll',  'srsvc.dll',    'urlmon.dll',   'user32.dll',
     'winhttp.dll',  'wininet.dll',  'ws2_32.dll',   'wship6.dll',
-    'advpack.dll',
+    'advpack.dll', 'ole32.dll', 'rstrtmgr.dll', 'iphlpapi.dll',
 ]
 
 VERBOSE=False
@@ -1049,6 +1049,99 @@ def shift0x82F63B78(inString,fName):
         val = ((v4 >> 1) ^ (0x82F63B78 * (v4 & 1))) & 0xffffffff
     return val ^ 0xBC
 
+pseudocode_contiApiHashing = '''Too hard to explain.\nAPI string hash function from Conti ransomware.\nSee code for information'''
+
+def contiApiHashing(inString, fName):
+    API_buffer = []
+
+    i = len(inString) >> 3
+
+    count = 0
+    while i != 0:
+        for index in range(0, 8):
+            API_buffer.append(inString[index + count])
+        count += 8
+        i -= 1
+
+    if len(inString) & 7 != 0:
+        v8 = len(inString) & 7
+
+        while v8 != 0:
+            API_buffer.append(inString[count])
+            count += 1
+            v8 -= 1
+
+    hash_val = 0
+
+    for i in range(0, len(API_buffer)):
+        API_buffer[i] = ord(API_buffer[i].lower())
+
+    v15 = 0xFF889912
+    string_length_2 = len(inString)
+    API_buffer_count = 0
+    if len(inString) >= 4:
+        count = string_length_2 >> 2
+        string_length_2 = (string_length_2 - 4 *
+                           (string_length_2 >> 2)) & 0xFFFFFFFF
+
+        while True:
+            temp_buffer_val = API_buffer[API_buffer_count +
+                                         3] << 24 | API_buffer[API_buffer_count +
+                                                               2] << 16 | API_buffer[API_buffer_count +
+                                                                                     1] << 8 | API_buffer[API_buffer_count]
+
+            temp = (0x5BD1E995 * temp_buffer_val) & 0xFFFFFFFF
+            API_buffer_count += 4
+            v15 = ((0x5BD1E995 * (temp ^
+                                  (temp >> 0x18))) & 0xFFFFFFFF) ^ ((0x5BD1E995 * v15) & 0xFFFFFFFF)
+            count -= 1
+            if count == 0:
+                break
+
+    v18 = string_length_2 - 1
+
+    v19 = v18 - 1
+
+    if v18 == 0:
+        hash_val ^= API_buffer[API_buffer_count]
+    elif v19 == 0:
+        hash_val ^= API_buffer[API_buffer_count + 1] << 8
+        hash_val ^= API_buffer[API_buffer_count]
+    elif v19 == 1:
+        hash_val ^= API_buffer[API_buffer_count + 2] << 16
+        hash_val ^= API_buffer[API_buffer_count + 1] << 8
+        hash_val ^= API_buffer[API_buffer_count]
+
+    v20 = (0x5BD1E995 * hash_val) & 0xFFFFFFFF
+    edi = (0x5BD1E995 * len(inString)) & 0xFFFFFFFF
+
+    eax = v20 >> 0x18
+    eax ^= v20
+
+    ecx = (0x5BD1E995 * eax) & 0xFFFFFFFF
+    eax = (0x5BD1E995 * v15) & 0xFFFFFFFF
+
+    ecx ^= eax
+
+    eax = edi
+    eax >>= 0x18
+    eax ^= edi
+
+    edx = (0x5BD1E995 * ecx) & 0xFFFFFFFF
+    eax = (0x5BD1E995 * eax) & 0xFFFFFFFF
+    edx ^= eax
+    eax = edx
+
+    eax >>= 0xD
+    eax ^= edx
+
+    ecx = (0x5BD1E995 * eax) & 0xFFFFFFFF
+    eax = ecx
+    eax >>= 0xF
+    eax ^= ecx
+
+    return eax
+
 ############################################################
 
 # The list of tuples of (supported hash name, hash size, pseudo_code)
@@ -1101,6 +1194,7 @@ HASH_TYPES = [
     ('fnv1Xor67f', 32, pseudocode_fnv1Xor67f),
     ('adler32_666', 32, 'Adler32 with starting value 666'),
     ('shift0x82F63B78',           32, 'like crc32c'),
+    ('contiApiHashing',       32, pseudocode_contiApiHashing)
 ]
 
 
